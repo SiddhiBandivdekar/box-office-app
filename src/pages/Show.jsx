81da591dbd9018857e4790b983e03180aaf88576
Siddhi <bandivdekarsiddhi@gmail.com>
2024-01-05 23:16:33 +0530

    installed react query. replaced useShowById with react query

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,30 +1,13 @@
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getShowById } from '../api/TvMaze';
-
-const useShowById = ({ showId }) => {
-  const [showData, setShowData] = useState(null);
-  const [showError, setShowError] = useState(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await getShowById(showId);
-        setShowData(response);
-      } catch (error) {
-        setShowError(error);
-      }
-    }
-
-    fetchData();
-  }, [showId]);
-
-  return { showData, showError };
-};
+import { useQuery } from 'react-query';
 
 const Show = () => {
   const { showId } = useParams();
-  const { showData, showError } = useShowById(showId);
+  const { data: showData, error: showError } = useQuery({
+    queryKey: ['show', showId],
+    queryFn: () => getShowById(showId),
+  });
 
   if (showError) {
     return <div>We have an error: {showError.message}</div>;
