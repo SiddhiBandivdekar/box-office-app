17ab4107719b6bc22bcb3f10f15d8a36d8d029f9
Siddhi <bandivdekarsiddhi@gmail.com>
2024-01-08 14:52:39 +0530

    created useStarredShows custom hook. placed useStarredShows look logic in a seperate file. tell user whether the show is starred or not

--- src/components/shows/ShowGrid.jsx
+++ src/components/shows/ShowGrid.jsx
@@ -1,37 +1,8 @@
-import { useEffect, useReducer } from 'react';
 import ShowCard from './ShowCard';
-
-const usePersistedReducer = (reducer, initialState, localStorageKey) => {
-  const [state, dispatch] = useReducer(reducer, initialState, initial => {
-    const persistedValue = localStorage.getItem(localStorageKey);
-
-    return persistedValue ? JSON.parse(persistedValue) : initial;
-  });
-
-  useEffect(() => {
-    localStorage.setItem(localStorageKey, JSON.stringify(state));
-  }, [state, localStorageKey]);
-
-  return [state, dispatch];
-};
-
-const starredReducer = (currentStarred, action) => {
-  switch (action.type) {
-    case 'STAR':
-      return currentStarred.concat(action.showId);
-    case 'UNSTAR':
-      return currentStarred.filter(showId => showId !== action.showId);
-    default:
-      return currentStarred;
-  }
-};
+import { useStarredShows } from '../../lib/useStarredShows';
 
 const ShowGrid = ({ shows }) => {
-  const [starredShows, dispatchStarredShows] = usePersistedReducer(
-    starredReducer,
-    [],
-    'starredShows'
-  );
+  const [starredShows, dispatchStarredShows] = useStarredShows();
   const onStarMeClick = showId => {
     const isStarred = starredShows.includes(showId);
 
@@ -51,6 +22,7 @@ const ShowGrid = ({ shows }) => {
             data.show.image ? data.show.image.medium : '/not-found-img.png'
           }
           onStarMeClick={onStarMeClick}
+          isStarred={starredShows.includes(data.show.id)}
         />
       ))}
     </div>
